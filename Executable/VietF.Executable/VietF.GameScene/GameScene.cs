c7a301097d77c82ef094916681e371cb95b38ffe
thienkhiem <itc.thienkhiem@gmail.com>
2014-09-16 18:07:45 +0700

    Menu
    Menu

---
+++ Executable/VietF.Executable/VietF.GameScene/GameScene.cs
@@ -0,0 +1,22 @@
+#region Using Statements
+using System;
+using WaveEngine.Common;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Services;
+#endregion
+
+namespace VietF.GameScene
+{
+    public class GameScene: Scene
+    {
+        protected override void Start()
+        {
+            base.Start();
+        }
+        protected override void CreateScene()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
